6810a1616625422d9facdf01a71beb0c3a597231
Averianov <averianovai@gmail.com>
2020-07-16 13:24:04 +0300

    aa-hw update

--- aa-hw-2/aa-hw-2.go
+++ aa-hw-2/aa-hw-2.go
@@ -1,15 +1,12 @@
 package aahw2
 
-// Complete the hourglassSum function below.
 func hourglassSum(arr [][]int32) int32 {
 	var max int32
 	max = -9 * 7
-	var count int
 
 	for i := 0; i < 4; i++ {
 		for j := 0; j < 4; j++ {
 			var sum int32
-			count++
 			sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
 			if max < sum {
 				max = sum
@@ -17,4 +14,21 @@ func hourglassSum(arr [][]int32) int32 {
 		}
 	}
 	return max
+}
+
+func rotLeft(a []int32, d int32) []int32 {
+	return append(a[d:], a[:d]...)
+}
+
+func alternatingCharacters(s string) int32 {
+	var last rune
+	result := int32(0)
+	for _, v := range s {
+		if v != last {
+			last = v
+		} else {
+			result++
+		}
+	}
+	return result
 }
--- aa-hw-2/aa-hw-2_test.go
+++ aa-hw-2/aa-hw-2_test.go
@@ -1,6 +1,7 @@
 package aahw2
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -55,5 +56,68 @@ func TestHourglassSum(t *testing.T) {
 			t.Errorf("Error Test hourglassSum. Iteration = %d, Want = %d and Got = '%v'.", i, tVal.want, result)
 		}
 
+	}
+}
+
+func TestRotLeft(t *testing.T) {
+
+	type testValue struct {
+		arr  []int32
+		d    int32
+		want []int32
+	}
+
+	var testValueArr []testValue
+
+	testValueArr = append(testValueArr, testValue{
+		[]int32{1, 2, 3, 4, 5},
+		4,
+		[]int32{5, 1, 2, 3, 4},
+	})
+
+	testValueArr = append(testValueArr, testValue{
+		[]int32{41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86, 51},
+		10,
+		[]int32{77, 97, 58, 1, 86, 58, 26, 10, 86, 51, 41, 73, 89, 7, 10, 1, 59, 58, 84, 77},
+	})
+
+	testValueArr = append(testValueArr, testValue{
+		[]int32{33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60, 87, 97},
+		13,
+		[]int32{87, 97, 33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60},
+	})
+
+	for i, tVal := range testValueArr {
+		result := rotLeft(tVal.arr, tVal.d)
+		if !reflect.DeepEqual(tVal.want, result) {
+			t.Errorf("Error Test hourglassSum. Iteration = %d, Want = %d and Got = '%v'.", i, tVal.want, result)
+		}
+	}
+}
+
+func TestAlternatingCharacters(t *testing.T) {
+
+	type testValue struct {
+		str  string
+		want int32
+	}
+
+	var testValueArr []testValue
+
+	testValueArr = append(testValueArr, testValue{"AAAA", 3})
+
+	testValueArr = append(testValueArr, testValue{"BBBBB", 4})
+
+	testValueArr = append(testValueArr, testValue{"ABABABAB", 0})
+
+	testValueArr = append(testValueArr, testValue{"BABABA", 0})
+
+	testValueArr = append(testValueArr, testValue{"AAABBB", 4})
+
+	for i, tVal := range testValueArr {
+		result := alternatingCharacters(tVal.str)
+		if tVal.want != result {
+			t.Errorf("Error Test hourglassSum. Iteration = %d, Want = %d and Got = '%v'.", i, tVal.want, result)
+		}
 	}
 }
