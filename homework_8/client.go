55ae5ce3585037333a9e6ff156770a8e00c57088
littlebossyporg <36607154+littlebossyporg@users.noreply.github.com>
2020-08-20 18:22:57 +0300

    Update client.go

--- homework_8/client.go
+++ homework_8/client.go
@@ -11,9 +11,7 @@ func main() {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Text to send: ")
 		text, _ := reader.ReadString('\n')
-		// Отправляем в socket
 		fmt.Fprintf(conn, text + "\n")
-		// Прослушиваем ответ
 		message, _ := bufio.NewReader(conn).ReadString('\n')
 		fmt.Print("Message from server: "+message)
 	}
